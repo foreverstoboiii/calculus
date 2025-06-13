4ff2277a182cdf514d2da178ad20984f24b21672
foreverstoboiii <shukurullayevnurmukhammad123@gmail.com>
2025-06-13 20:00:11 +0500

    Поправил стили некоторые

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function appendValue(symbol) {
     let display = document.getElementById("display")
     display.value += symbol;
 }
+//функция для дисплея 
 
 function calculate() {
     let display = document.getElementById('display');
     let result = eval(display.value);
     display.value = result;
 }
-
+//функция с eval() которая считает все что на дисплее
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+//функция для очистки дисплея 
