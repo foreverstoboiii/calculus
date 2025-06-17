04796865fcd81b7868b50d2799fe07170806b05b
Azamat <azamatahmadov31@gmail.com>
2025-06-17 14:26:40 +0500

    feat: работа с localStorage

--- index.js
+++ index.js
@@ -16,16 +16,30 @@ function clearDisplay() {
 //функция для очистки дисплея 
 
 const togglebutton = document.getElementById('themetoggle');
+const savedTheme = localStorage.getItem('theme');
+
+// При загрузке страницы устанавливаем тему из localStorage
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    togglebutton.textContent = '🌙'; // показываем иконку тёмной темы
+} else {
+    // по умолчанию светлая тема
+    document.body.classList.remove('dark-mode');
+    togglebutton.textContent = '🔆'; // иконка светлой темы
+}
 
 togglebutton.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 
     if (document.body.classList.contains('dark-mode')) {
         togglebutton.textContent = '🌙'; // Тёмная тема активна
+        localStorage.setItem('theme', 'dark'); // сохраняем выбор
     } else {
         togglebutton.textContent = '🔆'; // Светлая тема активна
+        localStorage.setItem('theme', 'light'); // сохраняем выбор
     }
 });
 
 //функция для переключение тем 
+
 
